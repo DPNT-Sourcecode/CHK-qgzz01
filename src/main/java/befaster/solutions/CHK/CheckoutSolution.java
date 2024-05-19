7da8591af47b5b4280d50db1f9a879d3ca5ae749
sbx_user1051 <sbx_user1051@169.254.134.29>
2024-05-19 15:42:59 +0000

    Sun May 19 15:42:59 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,45 +5,45 @@ import befaster.runner.SolutionNotImplementedException;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
     	int sum=0;
-    	int x=0;
-    	int y=0;
+    	int count_a=0;
+    	int count_b=0;
+    	int count_c=0;
+    	int count_d=0;
     	
-    	for(int i=0;i<skus.length();i++)
-    	{
-    		if(skus.charAt(i)=='A')
-    			x++;
-    		if(skus.charAt(i)=='B')
-    			y++;
-    	}
-    	
-    	
-    	if(x==3)
-    		sum=sum+130;
-    	if(y==2)
-    		sum=sum+45;
-    			
     	
         for( int i=0; i<skus.length();i++) {
         	switch(skus.charAt(i)) {
         	case 'A':
-        		if(x>=3)
-        			continue;
-        		sum=sum+50;
+        		count_a++;
         		break;
         	case 'B':
-        		if(y>=2)
-        			continue;
-        		sum=sum+30;
+        		count_b++;
         		break;
         	case 'C':
-        		sum=sum+20;
+        		count_c++;
         		break;
         	case 'D':
-        		sum=sum+15;
+        		count_d++;
         		break;
+        	default:
+        		return -1;
+        	
         	}
         }
+        
+        if(count_a>=3)
+        {
+        	if(count_b>=2)
+        		sum=sum+(count_a/3)*130+(count_b/2)*45+count_c*20+count_d*15;
+        	else
+        		sum=sum+(count_a/3)*130+count_b*30+count_c*20+count_d*15;
+        }
+        else
+        	sum=sum+count_a*50+count_b*45+count_c*20+count_d*15;
+        
+        
         return sum;
     }
         
 }
+
